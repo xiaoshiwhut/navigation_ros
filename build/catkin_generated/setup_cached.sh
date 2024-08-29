be9cbeb3843c56f0234c1f044e739bda06b3b0d9
syz <syz@whut.com>
2024-08-29 20:06:51 +0800

    jjjj

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/syz/move_base_qianjin/src/navigation-noetic-devel/base_local_planner/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/syz/move_base_qianjin/src/navigation-noetic-devel/base_local_planner/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/syz/move_base_qianjin/src/navigation-noetic-devel/base_local_planner/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/syz/move_base_qianjin/src/navigation-noetic-devel/base_local_planner/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/syz/move_base_qianjin/src/navigation-noetic-devel/base_local_planner/build'
+export PYTHONPATH="/home/syz/move_base_qianjin/src/navigation-noetic-devel/base_local_planner/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROS_PACKAGE_PATH="/home/syz/move_base_qianjin/src/navigation-noetic-devel/base_local_planner:$ROS_PACKAGE_PATH"
